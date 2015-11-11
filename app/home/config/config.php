1587faac4024ee8a833cf75b44c10241050e990c
chenshaozhong <lysily@vip.qq.com>
2015-11-11 09:46:37 +0800

    更新

--- app/home/config/config.php
+++ app/home/config/config.php
@@ -29,7 +29,7 @@ return [
 
     'WECHAT_APPID'=>'wx9467b53e5422b8dd',//微信appId
     'WECHAT_SECRET'=>'d9e676c8edf8f9cc01d63c49784f6f73',//微信secret
-    'TOKEN'=>'kissaoli',//微信secret
+    'TOKEN'=>'token',//微信token
     'EncodingAesKey'=>'1222222',//微信secret
     'SafeMode'=>1,//模式（1:明文模式,2：安全模式）
 
